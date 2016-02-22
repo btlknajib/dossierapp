4e040fbb4e2e91c1e6b22d696875e4f40f543c65
Btlknajib <boutellaka.najib@laposte.net>
2016-02-22 15:45:18 +0100

    update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, 'git@github.com:btlknajib/dossierapp.git'
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
 # Default deploy_to directory is /var/www/my_app
-set :deploy_to, 'home/deploy/dossier'
+set :deploy_to, '/home/deploy/dossier'
 
 # Default value for :scm is :git
 # set :scm, :git
